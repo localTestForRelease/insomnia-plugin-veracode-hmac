bcbef7e2dc71cafeec04fbd6159b585ec518a49b
Chris Campbell <chris@ctcampbell.com>
2019-02-12 11:44:32 +0000

    Fix url params bug

--- plugin.js
+++ plugin.js
@@ -9,6 +9,7 @@ const hosts = ['api.veracode.com', 'analysiscenter.veracode.com', 'api.veracode.
 module.exports.requestHooks = [
     context => {
         let url = new URL(context.request.getUrl());
+        let params = context.request.getParameters();
         
         if (url.protocol === 'https:' && hosts.includes(url.hostname)) {
             let authProfile = context.request.getEnvironmentVariable('veracode_auth_profile');
@@ -21,7 +22,14 @@ module.exports.requestHooks = [
             let id = config.get(authProfile, 'veracode_api_key_id');
             let key = config.get(authProfile, 'veracode_api_key_secret'); 
 
-            let header = hmac.calculateAuthorizationHeader(id, key, url.hostname, url.pathname, url.search, context.request.getMethod());
+            let paramStringInitialValue = url.search === '' ? '?' : url.search + '&';
+            let paramsString = params.reduce((accum, item, index, arr) => {
+                return accum + `${item.name}=${item.value}&`;
+            }, paramStringInitialValue);
+            paramsString = paramsString.slice(0, -1);
+            console.log(paramsString);
+
+            let header = hmac.calculateAuthorizationHeader(id, key, url.hostname, url.pathname, paramsString, context.request.getMethod());
             context.request.setHeader('Authorization', header);
         }
 
