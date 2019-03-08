ada12e3dacf8753f561dc8b51cc90238b6f10d68
Chris Campbell <chris@ctcampbell.com>
2019-03-08 16:11:20 +0000

    Add another SourceClear domain

--- plugin.js
+++ plugin.js
@@ -3,7 +3,7 @@ const os = require('os');
 const configparser = require('configparser');
 const hmac = require('./veracode-hmac.js');
 
-const hosts = ['api.veracode.com', 'analysiscenter.veracode.com', 'api.veracode.io', 'api.sourceclear.io'];
+const hosts = ['api.veracode.com', 'analysiscenter.veracode.com', 'api.veracode.io', 'api.sourceclear.io', 'api.sourceclear.com'];
 
 // Request hook to set header on every request
 module.exports.requestHooks = [
